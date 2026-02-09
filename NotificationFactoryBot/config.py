e686ba4818a3ff20529a72f147d8f642dca265af
aixra <aixra555@gmail.com>
2026-02-09 20:38:36 +0500

    Add files via upload

--- NotificationFactoryBot/config.py
+++ NotificationFactoryBot/config.py
@@ -1,12 +1,20 @@
-
-
-SECTOR_PREFIX = "Sector"
-BACK_BUTTON = "back"
+SECTOR_PREFIX = "Участок"
+DEVICE_PREFIC = "Device"
+BACK_BUTTON1 = "Назад"
+BACK_BUTTON2 = "Назад к сектору"
 
 ADMIN_CHAT_ID = -5228109324
 
 
-SECTOR1 = "Sector 1"
-SECTOR2 = "Sector 2"
-SECTOR3 = "Sector 3"
-SECTOR4 = "Sector 4"
+SECTOR1 = "Участок 1"
+SECTOR2 = "Участок 2"
+SECTOR3 = "Участок 3"
+SECTOR4 = "Участок 4"
+
+Device1 = "Device 1"
+Device2 = "Device 2"
+Device3 = "Device 3"
+device4 = "Device 4"
+
+ACTION_ADD_NOTE = "Добавить обращение"
+ACTION_BACK_TO_SECTORS = "Вернуться к выбору сектора"
--- NotificationFactoryBot/keyBoardReply.py
+++ NotificationFactoryBot/keyBoardReply.py
@@ -7,8 +7,24 @@ main = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text=config.SECTOR1), KeyboardButton(text=config.SECTOR2)],
         [KeyboardButton(text=config.SECTOR3), KeyboardButton(text=config.SECTOR4)],
-        [KeyboardButton(text=config.BACK_BUTTON)],
+        #[KeyboardButton(text=config.BACK_BUTTON1)],
     ],
     resize_keyboard=True,      # компактные кнопки
-    one_time_keyboard=False
+    one_time_keyboard=True
+)
+deviceChoose = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Device 1"), KeyboardButton(text="Device 2")],
+        [KeyboardButton(text="Device 3"), KeyboardButton(text="Device 4")],
+        [KeyboardButton(text=config.BACK_BUTTON2)],
+    ],
+    resize_keyboard=True,      # компактные кнопки
+    one_time_keyboard=True
+)
+Back = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text=config.BACK_BUTTON1)],
+    ],
+    resize_keyboard=True,      # компактные кнопки
+    one_time_keyboard=True
 )
